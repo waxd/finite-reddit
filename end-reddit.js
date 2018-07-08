75323307e7a6f7a767ae204df5751bbebcf4c43e
David Wax <david.allen.wax@gmail.com>
2018-07-07 22:08:27 -0700

    Add comments and organize end-reddit.js for readability.

--- end-reddit.js
+++ end-reddit.js
@@ -1,14 +1,24 @@
-var limitPromise = browser.storage.sync.get({
-    'redditLimit': 100
-});
-var rankNodes = document.getElementsByClassName("rank");
-var rankLimit = 100;
-limitPromise.then((result) => {
-    var rankLimit = Number(result.redditLimit);
+/*
+ * Hides all the posts above the provided rank limit.
+ */
+function filterPosts(rankLimit) {
+    var rankNodes = document.getElementsByClassName("rank");
     for (var i = 0; i < rankNodes.length; i++) {
 	var rank = Number(rankNodes[i].innerHTML);
+	/* non-number ranks are always hidden. */
 	if (isNaN(rank) || rank > rankLimit) {
 	    rankNodes[i].parentNode.setAttribute("hidden", "");
 	}
     }
+}
+
+// Read the Reddit limit from storage.
+var limitPromise = browser.storage.sync.get({
+    'redditLimit': 100
+});
+
+// Apply the rank limit to the posts on the page.
+limitPromise.then((result) => {
+    var rankLimit = Number(result.redditLimit);
+    filterPosts(rankLimit);
 });
