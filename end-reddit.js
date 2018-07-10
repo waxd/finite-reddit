c40b7cdf4a5a2d1bd01b90fe308bd7845c90dd02
David Wax <david.allen.wax@gmail.com>
2018-07-09 17:13:08 -0700

    Add comments to end-reddit.js.

--- end-reddit.js
+++ end-reddit.js
@@ -5,7 +5,7 @@ function filterPosts(rankLimit) {
     var rankNodes = document.getElementsByClassName("rank");
     for (var i = 0; i < rankNodes.length; i++) {
 	var rank = Number(rankNodes[i].innerHTML);
-	/* non-number ranks are always hidden. */
+	// non-number ranks are always hidden.
 	if (isNaN(rank) || rank > rankLimit) {
 	    rankNodes[i].parentNode.setAttribute("hidden", "");
 	}
@@ -17,11 +17,13 @@ var limitPromise = browser.storage.sync.get({
     'redditLimit': 100
 });
 
+// Read the disabled setting from storage.
 var disabledPromise = browser.storage.sync.get({
     'finiteRedditDisabled': false
 });
 
 disabledPromise.then((result) => {
+    // Don't change anything if the extension is disabled.
     if (!result.finiteRedditDisabled) {
 	// Apply the rank limit to the posts on the page.
 	limitPromise.then((result) => {
